cd58ad5258018df7e1d11296e23093c869b93096
chenjiabin <chenjiabin@stu01.(none)>
2018-08-16 11:50:47 +0800

    add expression

---
+++ bin/model/Model_train_FS_exp.R
@@ -0,0 +1,335 @@
+# please make sure required packages already installed
+
+
+# library packages
+require(mlbench)
+require(caret)
+require(DT)
+library(doParallel)
+library(PerfMeas)
+library(gplots)
+library(ROCR)
+library(randomForest)
+library(Metrics)
+
+# set arguments
+args <- commandArgs(T)
+cell <- args[1]
+PM <- args[2]
+cores <- as.numeric(args[3])
+#FIP_list <- args[5]
+seed <- 111
+
+# load data_train
+file <- paste(cell, 'exp_train', sep = '_')
+data_train_all <- read.table(file, head = T)
+data_train_all <- data.frame(data_train_all)
+data_train_raw <- data_train_all
+data_train_all$RPM <- log2(data_train_all$RPM)
+data_train <- data_train_all
+if (PM == 'glm') {
+	data_train$RPM <- data_train$RPM/10 # value = log2(RPM)/10
+}
+summary(data_train)
+set.seed(seed)
+print('>>> Dimension of data matrix: ')
+dim(data_train)
+n <- dim(data_train)[1]
+FN <- dim(data_train)[2] - 5
+col_name <- names(data_train)
+fea_all <- col_name[5:(FN+4)]
+#if (args[4] != 'all'){
+#		fn_list <- as.numeric(strsplit(args[4], ',')[[1]])
+#}
+# sort and reverse fn_list (decrease)
+#fn_list <- rev(sort(fn_list))
+#print('>>> Feature number list:')
+#print(fn_list) 
+#len_fn_list <- length(fn_list)
+#max_fn <- fn_list[1]
+data_train_mat <- data_train[,5:(FN+4)]
+
+# show which libraries were loaded  
+print('>>> Session Info: ')
+sessionInfo()
+
+# register parallel front-end
+cl <- makeCluster(cores); registerDoParallel(cl)
+
+ctrl <- trainControl(method = 'cv', number = 10 , savePredictions = "all", returnData = T, returnResamp = 'all', 
+					 verboseIter = T, allowParallel = T)
+
+# Train best function
+feature_sel <- function(fn) {
+	print('=====================================================')
+	print('    Sorted features: ')
+	print(sort_fea)
+	new_fea_list <<- sort_fea[1: (length(sort_fea)-1)]
+	print('    Selected new features: ')
+	print(new_fea_list)
+	print('    Feature number input: ')
+	print(fn)
+	if (fn == length(new_fea_list)) {
+		print('Temp feature number EQUAL to sorted feature number, PASS ==>> ')
+#		print(paste('>>> Top ', fn+1, ' features ==> ', sep = ''))
+#		print(sort_fea)
+	} else {
+		print('    Incoordinate temp feature number and sorted feature number ! ! ! ')
+	}
+	print('    Best features: ')
+	print(fea_best)
+	
+	data_train_mat_tmp <- data_train_mat[, new_fea_list]
+	## train model
+	Model_tmp <- train(y = data_train$RPM, x = data_train_mat_tmp, 
+				   method = PM, trControl = ctrl, metric = "RMSE", importance = T, preProc = c("center", "scale"))
+	
+	# Get results of resample
+	print('>>> summary of resample: ')
+	resample_tmp <- Model_tmp$resample
+	print(resample_tmp)
+	best_tune <- Model_tmp$bestTune
+	tunegrid <- expand.grid(best_tune)
+	tune_met <- colnames(best_tune)
+	print('>>> Best tune: ')
+	print(best_tune)
+
+	# tuned resample
+	resample_tune <- resample_tmp
+	for (i in 1:length(tune_met)) {
+		met <- tune_met[i]
+		met_val <- best_tune[,met]
+		resample_tune <- resample_tune[resample_tune[,met]==met_val,]
+	}
+	print('>>> Resamples of best tune: ')
+	print(resample_tune)
+
+	## Cross-validation resample
+	cv_perf_df_tmp <- cbind(resample_tune, rep(fn, 10), rep(cell, 10))
+	colnames(cv_perf_df_tmp) <- c(colnames(resample_tune), 'Fea_number', 'Cell_type')
+	cv_perf_df <<- rbind(cv_perf_df, cv_perf_df_tmp)
+	print('>>> New data frame of resample performance: ')
+	print(cv_perf_df)
+	write.table(cv_perf_df_tmp, paste(cell, PM, 'cv_perf', sep = '_'), row.names = F, col.names = F, 
+				append = T, quote = F, sep = '\t')
+	
+	## model performance (RMSE, R2)
+	if (PM == 'rf') {
+		mse_tmp <- mse(data_train$RPM, Model_tmp$finalModel$predicted)
+		rmse_tmp <- sqrt(mse_tmp)
+		SSE_tmp <- sum((Model_tmp$finalModel$predictedi - data_train$RPM)^2)
+		SST_tmp <- sum((data_train$RPM - mean(data_train$RPM)) ^ 2)
+		R2_tmp <- 1- SSE_tmp/SST_tmp
+		PCC_tmp <- cor.test(Model_tmp$finalModel$predicted, data_train$RPM,method = "pearson")
+		print('    Pearson\'s r (PCC): ')
+		print(PCC_tmp)
+	} else {
+		tmp_pred <- Model_tmp$pred
+		obs_tmp <- Model_tmp$pred$obs
+		for (i in 1:length(tune_met)) {
+			met <- tune_met[i]
+			met_val <- best_tune[,met]
+			tmp_pred <- tmp_pred[tmp_pred[,met]==met_val,]
+			obs_tmp <- tmp_pred$obs
+			pred_tmp <- tmp_pred$pred
+		}
+		mse_tmp <- mse(obs_tmp, pred_tmp)
+		rmse_tmp <- sqrt(mse_tmp)
+		SSE_tmp <- sum((pred_tmp - obs_tmp) ^ 2)
+		SST_tmp <- sum((data_train$RPM - mean(data_train$RPM)) ^ 2)
+		R2_tmp <- 1 - SSE_tmp/SST_tmp
+	}
+	print('    Model performance ==> ')
+	print(paste('    Total RMSE: ', rmse_tmp, sep = ''))
+	print(paste('    Total R2: ', R2_tmp, sep = ''))
+	
+	## feature importance
+	imp_tmp <- varImp(Model_tmp)
+	imp_tmp <- round(imp_tmp$importance, 2)
+	imp_tmp <- data.frame(imp_tmp)
+	order_imp_tmp <- order(imp_tmp[,'Overall'], decreasing = T)
+	sort_fea_tmp <- rownames(imp_tmp)[order_imp_tmp]
+	sort_imp_tmp <- data.frame(imp_tmp[,'Overall'][order_imp_tmp])
+	rownames(sort_imp_tmp) <- sort_fea_tmp
+	colnames(sort_imp_tmp) <- 'Importance'
+	print('    Sorted importance of MSE ==> ')
+	print(sort_imp_tmp)
+	print('    Summary of model: ')
+	print(Model_tmp)
+
+	## compare to previous best model
+	if (rmse_tmp < rmse_best) {
+		Model_best <<- Model_tmp
+		rmse_best <<- rmse_tmp
+		SSE_best <<- SSE_tmp
+		SST_best <<- SST_tmp
+		R2_best <<- R2_tmp
+		if (PM == 'rf') {
+			PCC_best <<- PCC_tmp
+		}
+		fea_best <<- new_fea_list
+		fn_best <<- fn
+		tune_best <<- best_tune
+	}
+
+	sort_fea <<- rownames(sort_imp_tmp)
+	print('   New rank of features: ')
+	print(sort_fea)
+
+	if (fn == 3) {
+		save(list = objects(), file=paste(cell, PM, 'FS_exp.RData', sep = '_'))
+	}
+
+	print('=====================================================')
+
+}
+
+
+print('>>> [1] Train model with all features ==> ')
+Model_all <- train(y = data_train$RPM, x = data_train_mat, 
+			   method = PM, trControl = ctrl, metric = "RMSE", importance = T, preProc = c("center", "scale"))
+Model_best <- Model_all
+
+# Get results of resample
+print('>>> Summary of resample (all features): ')
+resample_all <- Model_all$resample
+print(resample_all)
+best_tune_all <- Model_all$bestTune
+tune_best <- best_tune_all
+tunegrid <- expand.grid(best_tune_all)
+tune_met_all <- colnames(best_tune_all)
+print('>>> Best tune of all features: ')
+print(best_tune_all)
+
+# tuned resample
+resample_tune_all <- resample_all
+for (i in 1:length(tune_met_all)) {
+	met_all <- tune_met_all[i]
+	met_val_all <- best_tune_all[,met_all]
+	resample_tune_all <- resample_tune_all[resample_tune_all[,met_all]==met_val_all,]
+}
+print('>>> Resamples of best tune: ')
+print(resample_tune_all)
+
+## Cross-validation resample
+cv_perf_df <- cbind(resample_tune_all, rep(FN, 10), rep(cell, 10))
+colnames(cv_perf_df) <- c(colnames(resample_tune_all), 'Fea_number', 'Cell_type')
+write.table(cv_perf_df, paste(cell, PM, 'cv_perf', sep = '_'), row.names = F, col.names = T, quote = F, sep = '\t')
+
+## model performance of all feature (RMSE, R2)
+if (PM == 'rf') {
+	mmse <- mse(data_train$RPM, Model_all$finalModel$predicted)
+	rmse <- sqrt(mmse)
+	SSE <- sum((Model_all$finalModel$predicted - data_train$RPM)^2)
+	SST <- sum((data_train$RPM - mean(data_train$RPM)) ^ 2)
+	R2 <- 1- SSE/SST
+	PCC <- cor.test(Model_all$finalModel$predicted, data_train$RPM,method = "pearson")
+	PCC_best <- PCC
+	print('    Pearson\'s r (PCC): ')
+	print(PCC)
+} else {
+	all_pred <- Model_all$pred
+	for (i in 1:length(tune_met_all)) {
+		met_all <- tune_met_all[i]
+		met_val_all <- best_tune_all[,met_all]
+		all_pred <- all_pred[all_pred[,met_all]==met_val_all,]
+		obs_all <- all_pred$obs
+		pred_all <- all_pred$pred
+	}
+	mmse <- mse(obs_all, pred_all)
+	rmse <- sqrt(mmse)
+	SSE <- sum((pred_all - obs_all) ^ 2)
+	SST <- sum((data_train$RPM - mean(data_train$RPM)) ^ 2)
+	R2 <- 1 - SSE/SST
+}
+rmse_best <- rmse
+SSE_best <- SSE
+SST_best <- SST
+R2_best <- R2
+
+print('    Model performance ==> ')
+print(paste('    Total RMSE: ', rmse, sep = ''))
+print(paste('    Total R2: ', R2, sep = ''))
+
+## feature importance
+imp <- varImp(Model_all)
+imp <- round(imp$importance, 2)
+imp <- data.frame(imp)
+order_imp <- order(imp[,'Overall'], decreasing = T)
+sort_fea_all <- rownames(imp)[order_imp]
+sort_imp <- data.frame(imp[,'Overall'][order_imp])
+rownames(sort_imp) <- sort_fea_all
+colnames(sort_imp) <- 'Importance'
+sort_imp <- data.frame(sort_imp)
+colnames(sort_imp) <- c('Importance')
+#max_fea <- sort_fea_all[1:max_fn]
+fea_best <- sort_fea_all
+fn_best <- length(fea_best)
+sort_fea <- sort_fea_all
+write.table(sort_imp, paste(cell, PM, 'sort_Imp', sep = '_'), row.names = T, col.names = F, quote = F, sep = '\t')
+print('    Sorted importance of MSE ==> ')
+print(sort_imp)
+if (PM == 'rf') {
+	pdf(file = paste(cell, PM, "Imp.pdf", sep = "_"))
+	varImpPlot(Model_all$finalModel, type = 1, main = 'Feature Importance')
+	dev.off()
+}
+print('    Summary of model: ')
+print(Model_all)
+
+save(list = objects(), file=paste(cell, PM, 'train_pred_exp_allfea.RData', sep = '_'))
+
+
+print('>>> [2] Feature selsction ==> ')
+for (fn in seq(14,2)) {feature_sel(fn)}
+print('>>> Feature selsction finished. ')
+
+
+print('>>> [3] Summary of feature selection ==> ')
+print('    Best features: ')
+print(fea_best)
+print('    Number of best feature is: ')
+print(fn_best)
+print('    Model summary: ')
+print(Model_best)
+print('    Performance of best model ==> ')
+print(paste('    Total RMSE: ', rmse_best, sep = ''))
+print(paste('    Total R2: ', R2_best, sep = ''))
+if (PM == 'rf') {
+	print('    Pearson\'s r (PCC): ')
+	print(PCC_best)
+}
+tune_met_best <- colnames(tune_best)
+print('    Best tune of final model: ')
+print(tune_best)
+
+
+## write the observed and predicted expression to file
+if (PM == 'rf') {
+	obs_pred_exp <- data.frame(cbind(levels(data_train$Intron_pair), data_train$RPM, Model_best$finalModel$predicted))
+	colnames(obs_pred_exp) <- c('Intron_pair','true_exp', 'pred_exp')
+} else {
+	best_pred <- Model_best$pred
+	for (i in 1:length(tune_met_best)) {
+		met_best <- tune_met_best[i]
+		met_val_best <- tune_best[,met_best]
+		best_pred <- best_pred[best_pred[,met_best]==met_val_best,]
+		pred_best <- best_pred$pred
+		obs_best <- best_pred$obs
+		index_best <- best_pred$rowIndex
+	}
+	obs_pred_exp <- data.frame(cbind(levels(data_train[index_best,]$Intron_pair), obs_best, pred_best))
+	colnames(obs_pred_exp) <- c('Intron_pair', 'true_exp', 'pred_exp')
+}
+write.table(obs_pred_exp, paste(cell, PM, 'train_pred_exp', sep = '_'), row.names = F, col.names = T, quote = F, sep = '\t')
+
+# stop cluster and register sepuntial front end
+stopCluster(cl); registerDoSEQ()
+
+# save R data
+rm(list = 'args')
+save(list = objects(), file=paste(cell, PM, 'FS_exp.RData', sep = '_'))
+print('>>> Task DONE !!!')
+
+
+### END
---
+++ bin/model/Model_train_pred_exp.R
@@ -0,0 +1,219 @@
+# please make sure required packages already installed
+
+
+# library packages
+require(mlbench)
+require(caret)
+require(DT)
+library(doParallel)
+library(PerfMeas)
+library(gplots)
+library(ROCR)
+library(randomForest)
+library(Metrics)
+
+# set arguments
+args <- commandArgs(T)
+cell <- args[1]
+PM <- args[2]
+cores <- as.numeric(args[3])
+FIP_list <- args[5]
+seed <- 111
+
+# load data_train
+file <- paste(cell, 'exp_train', sep = '_')
+data_train_all <- read.table(file, head = T)
+data_train_all <- data.frame(data_train_all)
+data_train_raw <- data_train_all
+data_train_all$RPM <- log2(data_train_all$RPM)
+#data_train <- data_train_all[(2^(data_train_all$RPM)>0.01),] # remove low expression circRNAs (RPM > -6)
+data_train <- data_train_all
+if (PM == 'glm') {
+	data_train$RPM <- data_train$RPM/10 # value = log2(RPM)/10
+}
+summary(data_train)
+set.seed(seed)
+print('>>> Dimension of data matrix: ')
+dim(data_train)
+n <- dim(data_train)[1]
+FN <- dim(data_train)[2] - 5
+col_name <- names(data_train)
+fea_all <- col_name[5:(FN+4)]
+if (args[4] != 'all'){
+	fea_sel <- c(strsplit(args[4], ',')[[1]])
+} else {
+	fea_sel <- fea_all
+}
+data_train_mat <- data_train[,5:(FN+4)]
+sel_out <- 1:n
+for (i in 1:4)
+{
+	assign(paste('indextest', i, sep = ''), sort(sample(sel_out, n*0.2)))
+	assign(paste('indextrain', i, sep = ''), setdiff(1:n, get(paste('indextest', i, sep = ''))))
+	sel_out <- setdiff(sel_out, get(paste('indextest', i, sep = '')))
+	assign(paste('train', i, sep = ''), data.frame(data_train[get(paste('indextrain', i, sep = '')), ]))
+	assign(paste('test', i, sep = ''), data.frame(data_train[get(paste('indextest', i, sep = '')), ]))
+}
+indextest5 <- sel_out
+indextrain5 <- setdiff(1:n, indextest5)
+train5 <- data.frame(data_train[indextrain5, ])
+test5 <- data.frame(data_train[indextest5, ])
+
+# show which libraries were loaded  
+print('>>> Session Info: ')
+sessionInfo()
+
+# register parallel front-end
+cl <- makeCluster(cores); registerDoParallel(cl)
+
+print('>>> [1] Train model with circRNAs expression (RPM) of CIRCPedia ... ... ')
+ctrl <- trainControl(method = 'cv', number = 10 , savePredictions = "all", returnData = T, returnResamp = 'all', 
+					 verboseIter = T, allowParallel = T)
+#Model_all <- train(y = data_train$RPM, x = data_train_mat, method = PM, trControl = ctrl, preProc = c("center", "scale"))
+#Model <- train(y = data_train$RPM, x = data_train_mat[,c('Alu', 'H3K36me3', 'H3K79me2')], 
+Model <- train(y = data_train$RPM, 
+#			   x = data_train_mat[,c('Alu', 'DNaseI_HS', 'H3K27ac', 'H3K36me3', 'H3K4me1', 'H3K4me2', 'H3K4me3', 'H3K79me2')], 
+			   x = data_train_mat, 
+			   method = PM, trControl = ctrl, metric = "RMSE", importance = T, preProc = c("center", "scale"))
+
+## write the observed and predicted expression to file
+obs_pred_exp <- data.frame(cbind(levels(data_train$Intron_pair), data_train$RPM, Model$finalModel$predicted))
+colnames(obs_pred_exp) <- c('Intron_pair','true_exp', 'pred_exp')
+write.table(obs_pred_exp, paste(cell, PM, 'train_pred_exp', sep = '_'), row.names = F, col.names = T, quote = F, sep = '\t')
+
+## model performance (RMSE, R2)
+mse <- mse(data_train$RPM, Model$finalModel$predicted)
+rmse <- sqrt(mse)
+SSE <- sum((Model$finalModel$predicted - data_train$RPM)^2)
+SST <- sum((data_train$RPM - mean(data_train$RPM)) ^ 2)
+R2 <- 1- SSE/SST
+PCC <- cor.test(Model$finalModel$predicted, data_train$RPM, method = "pearson")
+print('    Model performance ==> ')
+print(paste('    Total RMSE: ', rmse, sep = ''))
+print(paste('    Total R2: ', R2, sep = ''))
+print('    Pearson\'s r (PCC): ')
+print(PCC)
+
+## feature importance
+if (PM == 'rf') {
+	imp <- importance(Model$finalModel)
+	write.table(imp, paste(cell, PM, 'Imp_all', sep = '_'), row.names = T, col.names = T, quote = F, sep = '\t')
+	print('    Importance ==> ')
+	print(imp)
+	sort_imp <- sort(importance(Model$finalModel)[,"%IncMSE"], decreasing = T)
+	sort_imp <- data.frame(sort_imp)
+	colnames(sort_imp) <- c('Importance')
+	write.table(sort_imp, paste(cell, PM, 'sort_Imp', sep = '_'), row.names = T, col.names = F, quote = F, sep = '\t')
+} else {
+	imp <- varImp(Model)
+	imp <- round(imp$importance, 2)
+	imp <- data.frame(imp)
+	order_imp <- order(imp[,'Overall'], decreasing = T)
+	sort_fea_all <- rownames(imp)[order_imp]
+	sort_imp <- data.frame(imp[,'Overall'][order_imp])
+	rownames(sort_imp) <- sort_fea_all
+	colnames(sort_imp) <- 'Importance'
+	sort_imp <- data.frame(sort_imp)
+	colnames(sort_imp) <- c('Importance')
+}
+print('    Sorted importance of MSE ==> ')
+print(sort_imp)
+if (PM == "rf") {
+	pdf(file = paste(cell, PM, "Imp.pdf", sep = "_"))
+	varImpPlot(Model$finalModel, type = 1, main = 'Feature Importance')
+	dev.off()
+}
+print('    Summary of model: ')
+print(Model)
+
+# stop cluster and register sepuntial front end
+stopCluster(cl); registerDoSEQ()
+
+# load pred data
+#load(paste('CELL', 'train_pred_exp.RData', sep = '_'))
+print('>>> [2] Predict circRNAs expression (RPM) with final model ... ... ')
+data_pred <- read.table(paste(cell, 'exp_pred', sep = '_'), head = T)
+data_pred.bed <- data_pred[1:4]
+all_pred_mat <- data_pred[-(1:3)]
+#sel_pred_mat <- all_pred_mat[,c('Alu','H3K36me3','H3K79me2')]
+#sel_pred_mat <- all_pred_mat[,c('Alu','DNaseI_HS','H3K27ac','H3K36me3','H3K4me1','H3K4me2','H3K4me3','H3K79me2')]
+sel_pred_mat <- all_pred_mat[,fea_sel]
+rownames(sel_pred_mat) <- data_pred$Intron_pair
+print('>>> Number of intron pairs and all feature:')
+print(paste(dim(all_pred_mat)[1], dim(all_pred_mat)[2]-2, sep = ';'))
+
+# prediction by trained model
+circ_pred <- predict(Model, sel_pred_mat)
+circ_pred <- 2^circ_pred
+circ_pred_df <- data.frame(circ_pred)
+circ_pred_bed <- data.frame(cbind(data_pred.bed, circ_pred_df))
+colnames(circ_pred_bed) <- c('Chr','Start','End','Intron_pair','RPM')
+#circ_pred_bed$RPM <- 2^(circ_pred_bed$RPM)
+#circ_pred_all <- predict(Model_all, all_pred_mat[fea_all])
+#circ_pred_all <- 2^circ_pred_all
+#circ_pred_all_df <- data.frame(circ_pred_all)
+#circ_pred_all_bed <- data.frame(cbind(data_pred.bed, circ_pred_all_df))
+#colnames(circ_pred_all_bed) <- c('Chr','Start','End','Intron_pair','RPM')
+max_exp <- max(circ_pred_df)
+min_exp <- min(circ_pred_df)
+max_exp_bed <- circ_pred_bed[c(which(circ_pred_df == max_exp)),]
+min_exp_bed <- circ_pred_bed[c(which(circ_pred_df == min_exp)),]
+#max_exp_df <- data.frame(cbind(max_exp_bed,data.frame(circ_pred[c(which(circ_pred_df == max_exp))])))
+#colnames(max_exp_df) <- c('Chr','Start','End','Intron_pair','RPM')
+#min_exp_df <- data.frame(cbind(min_exp_bed,data.frame(circ_pred[c(which(circ_pred_df == min_exp))])))
+#colnames(min_exp_df) <- c('Chr','Start','End','Intron_pair','RPM')
+print('    The max expressed circRNA:')
+print(max_exp_bed)
+print('    The min expressed circRNA:')
+print(min_exp_bed)
+
+write.table(circ_pred_bed, paste(cell, PM, 'pred_exp.bed', sep = '_'), row.names = F, col.names = F, quote = F, sep = '\t')
+#write.table(cbind(data_pred.bed[,1:4], circ_pred_all_df), paste(cell, PM, 'pred_exp_all.bed', sep = '_'), row.names = F, col.names = F, quote = F, append = T, sep = '\t')
+
+# save R data
+save(list = objects(), file=paste(cell, PM, 'train_pred_exp.RData', sep = '_'))
+
+# validate and evaluate the results by circBase data
+## read circBase circRNA FIP list
+print('>>> [3] Validate and evaluate the expression level of circRNAs ... ... ')
+CB_fip_list <- read.table(FIP_list, col.names='Intron_pair')
+CB_fip_list <- CB_fip_list$Intron_pair
+train_fip_list <- data_train_all$Intron_pair
+report_fip_list <- union(train_fip_list, CB_fip_list)
+col_name <- c('Chr', 'Start', 'End', 'Intron_pair', 'RPM')
+#colnames(circ_pred_all_bed) <- col_name
+all_exp_df <- data.frame(rbind(data_train_raw[,col_name], circ_pred_bed))
+all_fip_list <- all_exp_df$Intron_pair
+diff_fip_list <- setdiff(all_fip_list, report_fip_list) # un-reported circRNAs list
+print(paste('    Total of', length(CB_fip_list), 'known circRNAs (circBase) FIPs among all', length(all_fip_list), 'FIPs.', sep = ' '))
+CB_exp <- all_exp_df[all_exp_df$Intron_pair%in%CB_fip_list,]
+diff_exp <- all_exp_df[all_exp_df$Intron_pair%in%diff_fip_list,]
+write.table(CB_exp, paste(cell, PM, 'circbase_pred_exp', sep = '_'), row.names = F, col.names = F, quote = F, sep = '\t')
+write.table(diff_exp, paste(cell, PM, 'unreported_pred_exp', sep = '_'), row.names = F, col.names = F, quote = F, sep = '\t')
+
+
+## T-test of circBase expression level and others value
+print('>>> [4] T-test of circRNAs expression level between reported and un-reported groups ... ... ')
+print(paste('    Mean expression level of all genome:', mean(all_exp_df$RPM), sep = ' '))
+print(paste('    Mean value of reported and un-reported groups:', mean(CB_exp$RPM), ";", mean(diff_exp$RPM), sep = ' '))
+CB_RPM <- CB_exp$RPM
+diff_RPM <- diff_exp$RPM
+tt_RPM <- t.test(CB_RPM, diff_RPM, paired = F)
+print('    Summary of T-test: ')
+print(tt_RPM)
+print('    P-value: ')
+print(tt_RPM$p.value)
+if (tt_RPM$p.value < 0.05) {
+	print('    Significant !!!')
+} else {
+	print('    Not significant.')
+}
+
+print('>>> Task DONE !!!')
+
+# save R data
+rm(list = 'args')
+save(list = objects(), file=paste(cell, PM, 'train_pred_exp.RData', sep = '_'))
+
+
+### END
